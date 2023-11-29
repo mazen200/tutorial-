a97d8025526f1e5e080883ef0eb6102aa2577bdc
mazen <mazen15408@gmail.com>
2023-11-29 15:10:41 +0200

    events handling using scene builder

--- JavaFX/src/main/java/com/example/javafx/HelloController.java
+++ JavaFX/src/main/java/com/example/javafx/HelloController.java
@@ -3,17 +3,26 @@ package com.example.javafx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
+import javafx.scene.shape.Circle;
+
 public class HelloController {
+    @FXML
+    private Circle myCircle;
+    private double x,y;
     public void Up(ActionEvent e){
         System.out.println("UP");
+        myCircle.setCenterX(y+=10);
     }
     public void Down(ActionEvent e){
         System.out.println("down");
+        myCircle.setCenterY(y-=10);
     }
     public void left(ActionEvent e){
         System.out.println("left");
+        myCircle.setCenterX(x-=10);
     }
     public void right(ActionEvent e){
         System.out.println("right");
+        myCircle.setCenterX(x+=10) ;
     }
 }
