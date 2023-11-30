a11ee28a798b4839511d02c42afdfbc13c322d43
mazen <mazen15408@gmail.com>
2023-12-01 00:52:46 +0200

    DatePicker

--- JavaFX/src/main/java/com/example/javafx/Scene1Contoroller.java
+++ JavaFX/src/main/java/com/example/javafx/Scene1Contoroller.java
@@ -5,24 +5,20 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 public class Scene1Contoroller {
     @FXML
-    private Label myLabel;
+    private DatePicker myDatePicker;
     @FXML
-    private RadioButton rButton1, rButton2, rButton3;
+    private Label myLabel;
 
-    public void getFood(ActionEvent event) {
+    public void getDate(ActionEvent event) {
 
-        if(rButton1.isSelected()) {
-            myLabel.setText(rButton1.getText());
-        }
-        else if(rButton2.isSelected()) {
-            myLabel.setText(rButton2.getText());
-        }
-        else if(rButton3.isSelected()) {
-            myLabel.setText(rButton3.getText());
-        }
-
+        LocalDate myDate = myDatePicker.getValue();
+        String myFormattedDate = myDate.format(DateTimeFormatter.ofPattern("dd-MMM-yyyy"));
+        myLabel.setText(myFormattedDate);
     }
 }
