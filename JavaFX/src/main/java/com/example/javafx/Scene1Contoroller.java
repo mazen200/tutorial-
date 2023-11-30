bf94542b454b8fc1e4416f3a5c8c7b0160c457b1
mazen <mazen15408@gmail.com>
2023-12-01 01:16:11 +0200

    ChoiceBox

--- JavaFX/src/main/java/com/example/javafx/Scene1Contoroller.java
+++ JavaFX/src/main/java/com/example/javafx/Scene1Contoroller.java
@@ -3,22 +3,39 @@ package com.example.javafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class Scene1Contoroller {
-    @FXML
-    private Pane myPane;
-    @FXML
-    private ColorPicker myColorPicker;
+public class Scene1Contoroller implements Initializable {
 
-    public void changeColor(ActionEvent event) {
-        Color myColor = myColorPicker.getValue();
-        myPane.setBackground(new Background(new BackgroundFill(myColor, null, null)));
+        @FXML
+        private Label myLabel;
+
+        @FXML
+        private ChoiceBox<String> myChoiceBox;
+
+        private String[] food = {"pizza","sushi","ramen"};
+
+        @Override
+        public void initialize(URL arg0, ResourceBundle arg1) {
+
+            myChoiceBox.getItems().addAll(food);
+            myChoiceBox.setOnAction(this::getFood);
+
+        }
+
+        public void getFood(ActionEvent event) {
+
+            String myFood = myChoiceBox.getValue();
+            myLabel.setText(myFood);
+        }
+
     }
-}
