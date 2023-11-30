2888c91bc9b8e58995183cd757673c3a9f2767f9
mazen <mazen15408@gmail.com>
2023-11-30 02:07:28 +0200

    Exit button alert

--- JavaFX/src/main/java/com/example/javafx/HelloApplication.java
+++ JavaFX/src/main/java/com/example/javafx/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.javafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,9 +17,25 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-    }
+        stage.setOnCloseRequest(event -> {
+            event.consume();
+            logout(stage);
+        });
 
-    public static void main(String[] args) {
+    }
+    public void logout(Stage stage){
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You're about to logout!");
+        alert.setContentText("Do you want to save before exiting?");
+
+        if (alert.showAndWait().get() == ButtonType.OK){
+            System.out.println("You successfully logged out");
+            stage.close();
+        }
+    }
+        public static void main(String[] args) {
         launch();
     }
 }
